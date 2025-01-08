cf44043483be209dd418708dfb4d4ae0a0ec2b18
caspeer <mr.ash_x7@protonmail.ch>
2025-01-08 23:59:16 +0100

    array based stacks test

---
+++ stacks/testarr.pas
@@ -0,0 +1,17 @@
+
+program test;
+uses stackdsa;
+
+Begin
+	CreateStack();
+	Push(2003);
+	Push(8);
+	Push(3);
+	WriteLn(GetTop());
+	Pop();
+	WriteLn(GetTop());
+	Pop();
+	WriteLn(GetTop());
+End.
+
+
